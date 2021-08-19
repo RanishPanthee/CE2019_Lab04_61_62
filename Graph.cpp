f1eec65c2f9e99170e3ef14dac0890bdb49c9ae9
Sundar7577 <85982463+Sundar7577@users.noreply.github.com>
2021-08-19 17:44:50 +0545

    Some correction

--- Graph.cpp
+++ Graph.cpp
@@ -1,5 +1,4 @@
 #include<Graph.h>
-
 void Graph :: addvertex(int value){
   Vertex *ver=new Vertex(value);
   if (HEAD==NULL)  
@@ -14,38 +13,70 @@ void Graph :: addvertex(int value){
 }
 
 
+
+
+
 void Graph ::addedge(int vertex,int value)
-{
-    Edge *edg = new Edge(value);
-    Vertex *temp;
+{	Edge *edge = new Edge(value);
+    Vertex *temp = HEAD;
+    while(temp!= NULL  &&  temp->key != vertex){
+        temp = temp->down;
+    }
+    
+    if(temp == NULL){
+        cout<<"INVALD VERTEX"<<endl;
+    }
+    
+    else{
+        
+        Edge *edgePtr = temp->right;
+        if(edgePtr == NULL){
+            temp->right = edge;
+            cout<<endl<<"Edge Connected to : "<<vertex<<endl;
+        }
+        
+        else{
+            
+            while(edgePtr->next != NULL){
+                edgePtr = edgePtr->next;
+            }
+            
+            edgePtr->next=edge;
+            cout<<"Edge connected to : "<<vertex<<endl;
+            
+        }
+        
+    }
+	
+   
+}
+
+Vertex* Graph::searchvertex(int value){
+    
     Vertex *temp=HEAD;
-    while(temp->key!=Vertex && temp!=NULL){
+    
+    while(temp!= NULL  &&  temp->key != value){
+        
         temp=temp->down;
     }
-    if(temp!=NULL)
-    Edge *edgptr=temp->right;
-    if (edgptr==NULL)
-    {
-        temp->right=edg;
-        cout<<"Edge is connected to vertex"<<vertex<<endl;
-        else
-        while(edgptr->next!=NULL)
-        {
-            edgptr=edgptr->next;
-        }
-        edgptr->next=edg;
-        cout<<"Edge is connected to vertex:"<<vertex<<endl;
+    
+    if(temp==NULL){
+        return temp;
     }
+    
+    else{
+        return temp;
+    }
+    
 }
 
 
 
-
-int Graph :: outdegree(int vertex){
+int Graph::outdegree(int vertex){
     int x=0;
-    Vertex *ver = searchVertex(vertex);
-    Edge *edg=Ver->right;
-    if (edge!=NULL)
+    Vertex *ver = searchvertex(vertex);
+    Edge *edg=ver->right;
+    if (edg!=NULL)
     {
         while(edg!=NULL)
         {x++;
@@ -60,13 +91,13 @@ int Graph :: outdegree(int vertex){
 }
 
 
-int Graph :: indegree(int vertex){
+int Graph::indegree(int vertex){
     Vertex *temp=HEAD;
     int x=0;
     while(temp!=NULL)
     {
         Edge *edg=temp->right;
-        while(edg->key!=vertex && edg!=NULL){
+        while(edg!=NULL&& edg->key!=vertex){
             edg=edg->next;
         }
         if(edg!=NULL)
@@ -80,22 +111,167 @@ int Graph :: indegree(int vertex){
 }
 
 
-int Graph :: degree(int vertex)
+int Graph::degree(int vertex)
 {
     int x =indegree(vertex);
     int y =outdegree(vertex);
-    return9=(x+y);
+    return (x+y);
 }
 
 
 
-bool Graph :: areneighbours(int vertex1,int vertex2)
+bool Graph::areneighbours(int vertex1,int vertex2)
 {
-    Edge *edg1=vertex1->right;
-    Edge *edg2=vertex->right;
+    
+	Vertex *vertexx1=searchvertex(vertex1);
+    Vertex *vertexx2=searchvertex(vertex2);
+    
+	Edge *edg1=vertexx1->right;
+    Edge *edg2=vertexx2->right;
 
-    Vertex *vertex1=searchvertex(vertex1);
-    Vertex *vertex2=searchvertex(vertex2);
+    while(edg1->key!=vertex2 && edg1!=NULL)
+    {
+        edg1=edg1->next;
+    }
+    while(edg2->key!=vertex1 && edg2!=NULL)
+    {
+        edg2=edg2->next;
+    }
+ if(edg1!=NULL && edg2!=NULL)
+   return true;
+   else
+   return false;
+}
+void Graph :: addvertex(int value){
+  Vertex *ver=new Vertex(value);
+  if (HEAD==NULL)  
+  {
+      HEAD=ver;
+      TAIL=HEAD;
+  }
+  else{
+      TAIL->down=ver;
+      TAIL=TAIL->down;
+     }
+}
+
+
+
+
+
+void Graph ::addedge(int vertex,int value)
+{	Edge *edge = new Edge(value);
+    Vertex *temp = HEAD;
+    while(temp!= NULL  &&  temp->key != vertex){
+        temp = temp->down;
+    }
+    
+    if(temp == NULL){
+        cout<<"INVALD VERTEX"<<endl;
+    }
+    
+    else{
+        
+        Edge *edgePtr = temp->right;
+        if(edgePtr == NULL){
+            temp->right = edge;
+            cout<<endl<<"Edge Connected to : "<<vertex<<endl;
+        }
+        
+        else{
+            
+            while(edgePtr->next != NULL){
+                edgePtr = edgePtr->next;
+            }
+            
+            edgePtr->next=edge;
+            cout<<"Edge connected to : "<<vertex<<endl;
+            
+        }
+        
+    }
+	
+   
+}
+
+Vertex* Graph::searchvertex(int value){
+    
+    Vertex *temp=HEAD;
+    
+    while(temp!= NULL  &&  temp->key != value){
+        
+        temp=temp->down;
+    }
+    
+    if(temp==NULL){
+        return temp;
+    }
+    
+    else{
+        return temp;
+    }
+    
+}
+
+
+
+int Graph::outdegree(int vertex){
+    int x=0;
+    Vertex *ver = searchvertex(vertex);
+    Edge *edg=ver->right;
+    if (edg!=NULL)
+    {
+        while(edg!=NULL)
+        {x++;
+        edg==edg->next;
+        }
+        return x;
+    }
+    else{
+    return x;
+    }
+
+}
+
+
+int Graph::indegree(int vertex){
+    Vertex *temp=HEAD;
+    int x=0;
+    while(temp!=NULL)
+    {
+        Edge *edg=temp->right;
+        while(edg!=NULL&& edg->key!=vertex){
+            edg=edg->next;
+        }
+        if(edg!=NULL)
+        {
+            x++;
+        }
+        temp=temp->down;
+        return x;
+
+    }
+}
+
+
+int Graph::degree(int vertex)
+{
+    int x =indegree(vertex);
+    int y =outdegree(vertex);
+    return (x+y);
+}
+
+
+
+bool Graph::areneighbours(int vertex1,int vertex2)
+{
+    
+	Vertex *vertexx1=searchvertex(vertex1);
+    Vertex *vertexx2=searchvertex(vertex2);
+    
+	Edge *edg1=vertexx1->right;
+    Edge *edg2=vertexx2->right;
+
     while(edg1->key!=vertex2 && edg1!=NULL)
     {
         edg1=edg1->next;
